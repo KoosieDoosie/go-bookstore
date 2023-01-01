d9a3bbbdd1ef43dad0a226bc50c87f169f9f8dc8
KoosieDoosie <Koosiedoosie@gmail.com>
2023-01-01 12:24:44 -0500

    Add files via upload

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,22 @@
+package config
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "KoosieDoosie:Ss27262726/simplerest?charset=utf8&parseTime=True&loc=Local") // Open connection with the database
+	if err != nil {
+		panic(err)
+	}
+	db = d
+
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
