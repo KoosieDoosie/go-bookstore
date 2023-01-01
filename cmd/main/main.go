e789359af74b3550a6ef9477fa62f2947a3c158b
KoosieDoosie <Koosiedoosie@gmail.com>
2023-01-01 12:27:04 -0500

    Add files via upload

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"github.com/KoosieDoosie/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r) // points to routes functionality r = routes
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
